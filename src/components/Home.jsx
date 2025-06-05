e982ea0915e4811b3419145498852eb0dfe39364
Robert Wagner <dwe@dwe.mshome.net>
2025-06-05 09:50:28 +0000

    home rework

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 function Home({ isName, isLoggedIn }) {
-  const name = localStorage.getItem('username');
+    function capitalizeFirstLetter(text) {
+      if (!text) return '';
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+  const name =capitalizeFirstLetter(localStorage.getItem('username'));
+
  useEffect(() => {
     async function callServer() {
         try {
@@ -20,11 +25,35 @@ function Home({ isName, isLoggedIn }) {
   return (
     <>
       {isLoggedIn ? (
-        <h1>{name}, willkommen auf unserer Website!</h1>
+        <h1>Hallo {name}, <br />willkommen im internen Kochbuch!</h1>
       ) : (
         <Link to="/login">Bitte loggen Sie sich ein</Link>
       )}
-      <p>Wir freuen uns, Sie hier begrüßen zu dürfen.</p>
+      <br />
+      <p>Hier können Sie Ihre Rezepte erstellen, bearbeiten und löschen.</p>
+      <p>Sie können auch Rezepte von anderen Benutzern kommentieren.</p>
+      <p>Die Rezepte werden in einer Datenbank gespeichert und sind für alle Benutzer sichtbar.</p>
+      <p>Die Rezepte können Bilder enthalten, die Sie beim Erstellen des Rezepts hochladen können.</p>
+      <p>Die Rezepte können auch Zutatenlisten und Zubereitungsschritte enthalten.</p>
+      <p>Viel Spaß</p>
+      <p><img src='http://api-test.mshome.net:3001/default_Image/cooking1.jpg' width="500" height="100%" ></img></p>
+      <style>
+        {`
+          h1 {
+            text-align: center;
+            margin-top: 20px;
+          }
+          p {
+            text-align: center;
+            font-size: 18px;
+          }
+          img {
+            display: block;
+            margin: 0 auto;
+            border-radius: 39%;
+          }
+        `}
+      </style>
     </>
   );
 }
